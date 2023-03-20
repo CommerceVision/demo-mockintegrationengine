d8443da0f53a3f185a15ec5d026c55b8379eb22a
xinlibrisbane <porrimaxin@gmail.com>
2023-03-20 16:34:50 +1000

    Mock unit test

---
+++ FunctionTests/FunctionTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FunctionTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1, 0);
+        }
+    }
+}
